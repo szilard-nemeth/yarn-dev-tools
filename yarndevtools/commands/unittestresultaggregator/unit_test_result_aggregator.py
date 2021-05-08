fde1a21e68f7a496185de4ffa83ff93d8ec121a8
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-05-09 00:09:31 +0200

    unit_test_result_aggregator.py: Print summary info: Add all necessary classes to print summary.txt and summary.html files

--- yarndevtools/commands/unittestresultaggregator/unit_test_result_aggregator.py
+++ yarndevtools/commands/unittestresultaggregator/unit_test_result_aggregator.py
@@ -10,13 +10,19 @@ from googleapiwrapper.gmail_api import GmailWrapper, GmailThreads
 from googleapiwrapper.google_auth import GoogleApiAuthorizer
 from googleapiwrapper.google_sheet import GSheetOptions, GSheetWrapper
 from pythoncommons.file_utils import FileUtils
+from pythoncommons.html_utils import HtmlGenerator
 from pythoncommons.os_utils import OsUtils
 from pythoncommons.project_utils import ProjectUtils
-from pythoncommons.result_printer import BasicResultPrinter
-from pythoncommons.string_utils import RegexUtils
+from pythoncommons.result_printer import (
+    ResultPrinter,
+    TabulateTableFormat,
+    DEFAULT_TABLE_FORMATS,
+    GenericTableWithHeader,
+)
+from pythoncommons.string_utils import RegexUtils, StringUtils
 
 from yarndevtools.common.shared_command_utils import SECRET_PROJECTS_DIR
-from yarndevtools.constants import UNIT_TEST_RESULT_AGGREGATOR
+from yarndevtools.constants import UNIT_TEST_RESULT_AGGREGATOR, SUMMARY_FILE_TXT, SUMMARY_FILE_HTML
 
 SUBJECT = "subject:"
 
@@ -92,6 +98,7 @@ class UnitTestResultAggregatorConfig:
     def __str__(self):
         return (
             f"Full command was: {self.full_cmd}\n"
+            f"Console mode: {self.console_mode}\n"
             f"Gmail query: {self.gmail_query}\n"
             f"Smart subject query: {self.smart_subject_query}\n"
             f"Match expression: {self.match_expression}\n"
@@ -134,7 +141,7 @@ class UnitTestResultAggregator:
         threads: GmailThreads = self.gmail_wrapper.query_threads_with_paging(
             query=gmail_query, limit=self.config.request_limit, expect_one_message_per_thread=True
         )
-        match_objects = self.filter_data_by_regex_pattern(threads)
+        match_objects: List[MatchedLinesFromMessage] = self.filter_data_by_regex_pattern(threads)
         self.process_data(match_objects)
 
     def filter_data_by_regex_pattern(self, threads) -> List[MatchedLinesFromMessage]:
@@ -169,16 +176,30 @@ class UnitTestResultAggregator:
 
     def process_data(self, match_objects: List[MatchedLinesFromMessage]):
         truncate = self.config.operation_mode == OperationMode.PRINT
-        header = ["Date", "Subject", "Testcase", "Message ID", "Thread ID"]
-        converted_data: List[List[str]] = DataConverter.convert_data_to_rows(match_objects, truncate=truncate)
-        self.print_results_table(converted_data, header)
+        normal_table_header = ["Date", "Subject", "Testcase", "Message ID", "Thread ID"]
+        aggregated_table_header = ["Testcase", "Frequency of failures", "Latest failure"]
+        simple_match_result_rows: List[List[str]] = DataConverter.convert_data_to_rows(match_objects, truncate=truncate)
+        aggregated_match_result_rows: List[List[str]] = DataConverter.convert_data_to_aggregated_rows(match_objects)
+
+        table_config = (
+            TableConfig()
+            .add(TableDataType.MATCHED_LINES, [TableType.REGULAR, TableType.HTML])
+            .add(TableDataType.MATCHED_LINES_AGGREGATED, [TableType.REGULAR, TableType.HTML])
+        )
+        rendered_summary = RenderedSummary(
+            normal_table_header,
+            simple_match_result_rows,
+            aggregated_table_header,
+            aggregated_match_result_rows,
+            table_config,
+        )
+        output_manager = UnitTestResultOutputManager(self.config.session_dir, self.config.console_mode)
+        output_manager.print_and_save_summary(rendered_summary)
 
         if self.config.operation_mode == OperationMode.GSHEET:
             LOG.info("Updating Google sheet with data...")
-            header_aggregated = ["Testcase", "Frequency of failures", "Latest failure"]
-            aggregated_data: List[List[str]] = DataConverter.convert_data_to_aggregated_rows(match_objects)
-            self.update_gsheet(header, converted_data)
-            self.update_gsheet_aggregated(header_aggregated, aggregated_data)
+            self.update_gsheet(normal_table_header, simple_match_result_rows)
+            self.update_gsheet_aggregated(aggregated_table_header, aggregated_match_result_rows)
 
     @staticmethod
     def _check_if_line_is_valid(line, skip_lines_starting_with):
@@ -188,10 +209,6 @@ class UnitTestResultAggregator:
                 valid_line = False
                 break
         return valid_line
-
-    @staticmethod
-    def print_results_table(data, header):
-        BasicResultPrinter.print_table(data, header)
 
     def update_gsheet(self, header, data):
         self.gsheet_wrapper_normal.write_data(header, data, clear_range=False)
@@ -217,6 +234,175 @@ class UnitTestResultAggregator:
                 )
                 return new_query
         return orig_query
+
+
+class TableType(Enum):
+    REGULAR = "regular"
+    HTML = "html"
+    REGULAR_WITH_COLORS = "regular_colorized"
+
+
+class TableDataType(Enum):
+    MATCHED_LINES = ("matches lines per thread", "MATCHED LINES PER MAIL THREAD")
+    MATCHED_LINES_AGGREGATED = ("matches lines aggregated", "MATCHED LINES AGGREGATED")
+
+    def __init__(self, key, header_value):
+        self.key = key
+        self.header = header_value
+
+
+class TableConfig:
+    def __init__(self):
+        self.tables: Dict[TableDataType, List[TableType]] = {}
+
+    def add(self, data_type, table_type):
+        if data_type not in self.tables:
+            self.tables[data_type] = []
+        self.tables[data_type].extend(table_type)
+        return self
+
+
+class RenderedSummary:
+    def __init__(
+        self,
+        normal_table_header: List[str],
+        simple_match_result_rows: List[List[str]],
+        aggregated_table_header: List[str],
+        aggregated_match_result_rows: List[List[str]],
+        table_config: TableConfig,
+    ):
+        self.simple_table_header = normal_table_header
+        self.simple_match_result_rows = simple_match_result_rows
+        self.aggregated_table_header = aggregated_table_header
+        self.aggregated_match_result_rows = aggregated_match_result_rows
+        self.table_config = table_config
+
+        self._tables: Dict[TableDataType, List[GenericTableWithHeader]] = {}
+        self.add_matched_lines_result_table()
+        self.add_matched_lines_result_table_aggregated()
+        self.printable_summary_str, self.html_summary = self.generate_summary_msgs()
+
+    @property
+    def writable_summary_str(self):
+        # Writable / Printable tables are the same: no colorization implementation yet
+        return self.printable_summary_str
+
+    def get_tables(
+        self, ttype: TableDataType, colorized: bool = False, table_fmt: TabulateTableFormat = TabulateTableFormat.GRID
+    ):
+        tables = self._tables[ttype]
+        return list(filter(lambda t: t.colorized == colorized and t.table_fmt == table_fmt, tables))
+
+    def add_matched_lines_result_table(self):
+        self._add_table_internal(self.simple_table_header, self.simple_match_result_rows, TableDataType.MATCHED_LINES)
+
+    def add_matched_lines_result_table_aggregated(self):
+        self._add_table_internal(
+            self.aggregated_table_header, self.aggregated_match_result_rows, TableDataType.MATCHED_LINES_AGGREGATED
+        )
+
+    def _add_table_internal(self, header: List[str], data: List[List[str]], dtype: TableDataType):
+        gen_tables = ResultPrinter.print_tables(
+            data,
+            lambda row: row,
+            header=header,
+            print_result=False,
+            max_width=200,
+            max_width_separator=" ",
+            tabulate_fmts=DEFAULT_TABLE_FORMATS,
+        )
+        for table_fmt, table in gen_tables.items():
+            self._add_table(dtype, GenericTableWithHeader(dtype.header, table, table_fmt=table_fmt, colorized=False))
+
+    def _add_table(self, dtype: TableDataType, table: GenericTableWithHeader):
+        if dtype not in self._tables:
+            self._tables[dtype] = []
+        self._tables[dtype].append(table)
+
+    def generate_summary_msgs(self):
+        def regular_table(dt: TableDataType):
+            return self.get_tables(dt, table_fmt=TabulateTableFormat.GRID, colorized=False)
+
+        def regular_colorized_table(dt: TableDataType):
+            return self.get_tables(dt, table_fmt=TabulateTableFormat.GRID, colorized=True)
+
+        def html_table(dt: TableDataType):
+            return self.get_tables(dt, table_fmt=TabulateTableFormat.HTML, colorized=False)
+
+        printable_tables: List[GenericTableWithHeader] = []
+        html_tables: List[GenericTableWithHeader] = []
+        for table_data_type, table_types in self.table_config.tables.items():
+            for tt in table_types:
+                if tt == TableType.REGULAR:
+                    printable_tables.extend(regular_table(table_data_type))
+                elif tt == TableType.HTML:
+                    html_tables.extend(html_table(table_data_type))
+                elif tt == TableType.REGULAR_WITH_COLORS:
+                    printable_tables.extend(regular_colorized_table(table_data_type))
+        return (
+            self._generate_summary_str(printable_tables),
+            self.generate_summary_html(html_tables),
+        )
+
+    @staticmethod
+    def _generate_summary_str(tables):
+        printable_summary_str: str = ""
+        for table in tables:
+            printable_summary_str += str(table)
+            printable_summary_str += "\n\n"
+        return printable_summary_str
+
+    @staticmethod
+    def generate_summary_html(html_tables) -> str:
+        table_tuples = [(h.header, h.table) for h in html_tables]
+
+        html_sep = HtmlGenerator.generate_separator(tag="hr", breaks=2)
+        return (
+            HtmlGenerator()
+            .begin_html_tag()
+            .add_basic_table_style()
+            .append_html_tables(
+                table_tuples, separator=html_sep, header_type="h1", additional_separator_at_beginning=True
+            )
+            .render()
+        )
+
+
+class UnitTestResultOutputManager:
+    def __init__(self, output_dir, console_mode):
+        self.output_dir = output_dir
+        self.console_mode = console_mode
+
+    def _write_to_file_or_console(self, contents, output_type, add_sep_to_end=False):
+        if self.console_mode:
+            LOG.info(f"Printing {output_type}: {contents}")
+        else:
+            fn_prefix = self._convert_output_type_str_to_file_prefix(output_type, add_sep_to_end=add_sep_to_end)
+            f = self._generate_filename(self.output_dir, fn_prefix)
+            LOG.info(f"Saving {output_type} to file: {f}")
+            FileUtils.save_to_file(f, contents)
+
+    def print_and_save_summary(self, rendered_summary: RenderedSummary):
+        LOG.info(rendered_summary.printable_summary_str)
+
+        filename = FileUtils.join_path(self.output_dir, SUMMARY_FILE_TXT)
+        LOG.info(f"Saving summary to text file: {filename}")
+        FileUtils.save_to_file(filename, rendered_summary.writable_summary_str)
+
+        filename = FileUtils.join_path(self.output_dir, SUMMARY_FILE_HTML)
+        LOG.info(f"Saving summary to html file: {filename}")
+        FileUtils.save_to_file(filename, rendered_summary.html_summary)
+
+    @staticmethod
+    def _convert_output_type_str_to_file_prefix(output_type, add_sep_to_end=True):
+        file_prefix: str = output_type.replace(" ", "-")
+        if add_sep_to_end:
+            file_prefix += "-"
+        return file_prefix
+
+    @staticmethod
+    def _generate_filename(basedir, prefix, branch_name="") -> str:
+        return FileUtils.join_path(basedir, f"{prefix}{StringUtils.replace_special_chars(branch_name)}")
 
 
 class DataConverter:
