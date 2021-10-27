13629e59c3e51e8f52b114b19adf6f0917a455a0
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-10-27 23:35:16 +0200

    Add initial-cdsw-setup.sh script

--- yarndevtools/cdsw/scripts/clone_downstream_repos.sh
+++ yarndevtools/cdsw/scripts/clone_downstream_repos.sh
@@ -32,7 +32,7 @@ REPOS_ROOT="/home/cdsw/repos"
 clone-fetch-yarndevtools
 clone-fetch-hadoop-downstream
 
-# Install python requirements
+# Install python requirements with the latest version of the requirements file
 ##NOTE: yarndevtools will be installed as a python module so it won't fail with:
 curl -o /tmp/requirements-cdsw.txt https://raw.githubusercontent.com/szilard-nemeth/yarn-dev-tools/master/yarndevtools/cdsw/requirements.txt
 pip3 install -r /tmp/requirements-cdsw.txt --force-reinstall
