21113f23c3358f3f7aa484eec9a7d71108202d25
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-10-27 23:56:58 +0200

    initial-cdsw-setup.sh: Use fixed scripts dir instead of temp dir

--- yarndevtools/cdsw/scripts/initial-cdsw-setup.sh
+++ yarndevtools/cdsw/scripts/initial-cdsw-setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -x
-mkdir /tmp/yarn-cdsw-setup
 
 echo "Downloading clone repository scripts..."
-curl -o /tmp/yarn-cdsw-setup/clone_downstream_repos.sh https://raw.githubusercontent.com/szilard-nemeth/yarn-dev-tools/master/yarndevtools/cdsw/scripts/clone_downstream_repos.sh
-curl -o /tmp/yarn-cdsw-setup/clone_upstream_repos.sh https://raw.githubusercontent.com/szilard-nemeth/yarn-dev-tools/master/yarndevtools/cdsw/scripts/clone_upstream_repos.sh
+SCRIPTS_DIR="/home/cdsw/scripts"
+mkdir $SCRIPTS_DIR
+curl -o $SCRIPTS_DIR/clone_downstream_repos.sh https://raw.githubusercontent.com/szilard-nemeth/yarn-dev-tools/master/yarndevtools/cdsw/scripts/clone_downstream_repos.sh
+curl -o $SCRIPTS_DIR/clone_upstream_repos.sh https://raw.githubusercontent.com/szilard-nemeth/yarn-dev-tools/master/yarndevtools/cdsw/scripts/clone_upstream_repos.sh
 
 echo "Cloning upstream repos..."
-/tmp/yarn-cdsw-setup/clone_upstream_repos.sh
+$SCRIPTS_DIR/clone_upstream_repos.sh
 
 echo "Cloning downstream repos..."
-/tmp/yarn-cdsw-setup/clone_downstream_repos.sh
+$SCRIPTS_DIR/clone_downstream_repos.sh
 
 GLOBAL_SITE_PACKAGES=$(python3 -c 'import site; print(site.getsitepackages()[0])')
 USER_SITE_PACKAGES=$(python3 -m site --user-site)
@@ -18,7 +19,7 @@ USER_SITE_PACKAGES=$(python3 -m site --user-site)
 echo "Global python packages: $(ls -la $GLOBAL_SITE_PACKAGES)"
 echo "User python packages: $(ls -la $USER_SITE_PACKAGES)"
 
-#set up some convenience variables
+#Set up some convenience variables
 CDSW_PACKAGE_ROOT="$USER_SITE_PACKAGES/yarndevtools/cdsw"
 JOBS_ROOT=/home/cdsw/jobs/
 JOB_DS_BRANCHDIFF_REPORTING="downstream-branchdiff-reporting"
