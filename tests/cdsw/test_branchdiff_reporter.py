706ce9f8256e3ca0cb1c622e33afef65e2bd3e98
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-10-29 16:48:34 +0200

    test_branchdiff_reporter.py: Modify docker test to run in CDSW-like environment - Python package instead of downloaded source code

--- tests/cdsw/test_branchdiff_reporter.py
+++ tests/cdsw/test_branchdiff_reporter.py
@@ -6,39 +6,114 @@ from enum import Enum
 from typing import Dict
 from pythoncommons.docker_wrapper import DockerTestSetup
 from pythoncommons.file_utils import FileUtils, FindResultType
+from pythoncommons.object_utils import ObjUtils
 from pythoncommons.os_utils import OsUtils
 from pythoncommons.process import SubprocessCommandRunner
 from pythoncommons.project_utils import PROJECTS_BASEDIR_NAME
 
-from yarndevtools.cdsw.common_python.cdsw_common import (
-    HADOOP_CLOUDERA_BASEDIR,
-    CDSW_BASEDIR,
-    YARN_DEV_TOOLS_MODULE_ROOT,
-    YARN_DEV_TOOLS_SCRIPTS_BASEDIR,
-    HADOOP_UPSTREAM_BASEDIR,
-)
+from yarndevtools.cdsw.common_python.cdsw_common import CommonDirs, PythonModuleMode
 from yarndevtools.cdsw.common_python.constants import CdswEnvVar, BRANCH_DIFF_REPORTER_DIR_NAME, BranchComparatorEnvVar
 from yarndevtools.common.shared_command_utils import RepoType, EnvVar
+from yarndevtools.constants import ORIGIN_BRANCH_3_3, ORIGIN_TRUNK
+
 
 CREATE_IMAGE = True
-
-YARN_DEV_TOOLS_OUTPUT_CONTAINER_DIR = FileUtils.join_path("root", PROJECTS_BASEDIR_NAME, "yarn_dev_tools")
 PROJECT_NAME = "yarn-cdsw-branchdiff-reporting"
 PROJECT_VERSION = "1.0"
+DOCKER_IMAGE = f"szyszy/{PROJECT_NAME}:{PROJECT_VERSION}"
+
 MOUNT_MODE_RW = "rw"
 PYTHON3 = "python3"
 BASH = "bash"
 CDSW_DIRNAME = "cdsw"
 REPO_ROOT_DIRNAME = "yarn-dev-tools"
 CDSW_RUNNER_PY = "cdsw_runner.py"
-BRANCH_DIFF_SCRIPT_CONTAINER = FileUtils.join_path(
-    YARN_DEV_TOOLS_SCRIPTS_BASEDIR, BRANCH_DIFF_REPORTER_DIR_NAME, CDSW_RUNNER_PY
-)
-DOCKER_IMAGE = f"szyszy/{PROJECT_NAME}:{PROJECT_VERSION}"
 LOG = logging.getLogger(__name__)
 CMD_LOG = logging.getLogger(__name__)
-
 CONTAINER_SLEEP = 300
+INITIAL_CDSW_SETUP_SCRIPT = "initial-cdsw-setup.sh"
+
+
+class ContainerFiles:
+    BRANCH_DIFF_SCRIPT = FileUtils.join_path(
+        CommonDirs.YARN_DEV_TOOLS_JOBS_BASEDIR, BRANCH_DIFF_REPORTER_DIR_NAME, CDSW_RUNNER_PY
+    )
+    INITIAL_CDSW_SETUP_SCRIPT = FileUtils.join_path(
+        CommonDirs.YARN_DEV_TOOLS_SCRIPTS_BASEDIR, INITIAL_CDSW_SETUP_SCRIPT
+    )
+
+
+class ContainerDirs:
+    YARN_DEV_TOOLS_OUTPUT_DIR = FileUtils.join_path("root", PROJECTS_BASEDIR_NAME, "yarn_dev_tools")
+    CDSW_BASEDIR = CommonDirs.CDSW_BASEDIR
+    YARN_DEV_TOOLS_SCRIPTS_BASEDIR = CommonDirs.YARN_DEV_TOOLS_SCRIPTS_BASEDIR
+    HADOOP_CLOUDERA_BASEDIR = CommonDirs.HADOOP_CLOUDERA_BASEDIR
+    HADOOP_UPSTREAM_BASEDIR = CommonDirs.HADOOP_UPSTREAM_BASEDIR
+
+
+class LocalDirs:
+    REPO_ROOT_DIR = FileUtils.find_repo_root_dir(__file__, REPO_ROOT_DIRNAME)
+    CDSW_ROOT_DIR = None
+    SCRIPTS_DIR = None
+    YARNDEVTOOLS_RESULT_DIR = None
+
+
+class DockerMounts:
+    def __init__(self, docker_test_setup, exec_mode, python_module_mode):
+        self.docker_test_setup = docker_test_setup
+        self.exec_mode = exec_mode
+        self.python_module_mode = python_module_mode
+
+    def setup_env_vars(self):
+        # !! WARNING: User-specific settings below !!
+        if self.exec_mode == TestExecMode.CLOUDERA:
+            # We need both upstream / downstream repos for Cloudera-mode
+            os.environ[CdswEnvVar.CLOUDERA_HADOOP_ROOT.value] = FileUtils.join_path(
+                CommonDirs.USER_DEV_ROOT, "cloudera", "hadoop"
+            )
+            os.environ[CdswEnvVar.HADOOP_DEV_DIR.value] = FileUtils.join_path(
+                CommonDirs.USER_DEV_ROOT, "apache", "hadoop"
+            )
+        elif self.exec_mode == TestExecMode.UPSTREAM:
+            os.environ[CdswEnvVar.HADOOP_DEV_DIR.value] = FileUtils.join_path(
+                CommonDirs.USER_DEV_ROOT, "apache", "hadoop"
+            )
+            os.environ[BranchComparatorEnvVar.REPO_TYPE.value] = RepoType.UPSTREAM.value
+            os.environ[BranchComparatorEnvVar.FEATURE_BRANCH.value] = ORIGIN_BRANCH_3_3
+            os.environ[BranchComparatorEnvVar.MASTER_BRANCH.value] = ORIGIN_TRUNK
+
+        if self.python_module_mode == PythonModuleMode.GLOBAL:
+            os.environ[CdswEnvVar.PYTHON_MODULE_MODE.value] = PythonModuleMode.GLOBAL.value
+        elif self.python_module_mode == PythonModuleMode.USER:
+            os.environ[CdswEnvVar.PYTHON_MODULE_MODE.value] = PythonModuleMode.USER.value
+
+    def setup_default_docker_mounts(self):
+        self.setup_env_vars()
+        # Mount scripts dir, initial-cdsw-setup.sh will be executed from there
+        self.docker_test_setup.mount_dir(
+            LocalDirs.SCRIPTS_DIR, ContainerDirs.YARN_DEV_TOOLS_SCRIPTS_BASEDIR, mode=MOUNT_MODE_RW
+        )
+        # Mount results dir so all output files will be available on the host
+        self.docker_test_setup.mount_dir(
+            LocalDirs.YARNDEVTOOLS_RESULT_DIR, ContainerDirs.YARN_DEV_TOOLS_OUTPUT_DIR, mode=MOUNT_MODE_RW
+        )
+        if self.exec_mode == TestExecMode.CLOUDERA:
+            self._mount_downstream_hadoop_repo()
+            self._mount_upstream_hadoop_repo()
+        elif self.exec_mode == TestExecMode.UPSTREAM:
+            self._mount_upstream_hadoop_repo()
+
+    def _mount_downstream_hadoop_repo(self):
+        # Mount local Cloudera Hadoop dir so that container won't clone the repo again and again
+        self.docker_test_setup.mount_dir(
+            os.environ[CdswEnvVar.CLOUDERA_HADOOP_ROOT.value], ContainerDirs.HADOOP_CLOUDERA_BASEDIR, mode=MOUNT_MODE_RW
+        )
+
+    def _mount_upstream_hadoop_repo(self):
+        # Mount local upstream Hadoop dir so that container won't clone the repo again and again
+        self.docker_test_setup.mount_dir(
+            os.environ[CdswEnvVar.HADOOP_DEV_DIR.value], ContainerDirs.HADOOP_UPSTREAM_BASEDIR, mode=MOUNT_MODE_RW
+        )
 
 
 class TestExecMode(Enum):
@@ -47,15 +122,54 @@ class TestExecMode(Enum):
 
 
 class YarnCdswBranchDiffTests(unittest.TestCase):
+    python_module_mode = None
+    exec_mode = None
+    docker_test_setup = None
+    docker_mounts = None
+
     @classmethod
     def setUpClass(cls):
-        # Test expects that MAIL_ACC_PASSWORD is set with env var
         if CdswEnvVar.MAIL_ACC_PASSWORD.value not in os.environ:
             raise ValueError(f"Please set '{CdswEnvVar.MAIL_ACC_PASSWORD.value}' env var and re-run the test!")
         cls._setup_logging()
-        cls.repo_root_dir = FileUtils.find_repo_root_dir(__file__, REPO_ROOT_DIRNAME)
+        cls.setup_local_dirs()
+        cls.exec_mode: TestExecMode = cls.determine_execution_mode()
+        cls.python_module_mode = PythonModuleMode.GLOBAL
+        cls.docker_test_setup = DockerTestSetup(
+            DOCKER_IMAGE, create_image=CREATE_IMAGE, dockerfile_location=LocalDirs.CDSW_ROOT_DIR, logger=CMD_LOG
+        )
+        cls.docker_mounts = DockerMounts(cls.docker_test_setup, cls.exec_mode, cls.python_module_mode)
+        cls.docker_mounts.setup_default_docker_mounts()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        pass
+
+    @classmethod
+    def setup_local_dirs(cls):
+        LocalDirs.CDSW_ROOT_DIR = cls.get_cdsw_root_dir()
+        LocalDirs.SCRIPTS_DIR = FileUtils.join_path(LocalDirs.CDSW_ROOT_DIR, "scripts")
+        LocalDirs.YARNDEVTOOLS_RESULT_DIR = FileUtils.join_path(LocalDirs.CDSW_ROOT_DIR, "yarndevtools-results")
+        # TODO
+        cls.branchdiff_cdsw_runner_script = YarnCdswBranchDiffTests.find_cdsw_runner_script(
+            os.path.join(LocalDirs.CDSW_ROOT_DIR, BRANCH_DIFF_REPORTER_DIR_NAME)
+        )
+        # LOG.info("Local files: %s", ObjUtils.get_static_fields_with_values(LocalFiles))
+        LOG.info("Local dirs: %s", ObjUtils.get_static_fields_with_values(LocalDirs))
+        LOG.info("Container files: %s", ObjUtils.get_static_fields_with_values(ContainerFiles))
+        LOG.info("Container dirs: %s", ObjUtils.get_static_fields_with_values(ContainerDirs))
+
+    @classmethod
+    def determine_execution_mode(cls):
+        exec_mode_env: str = OsUtils.get_env_value(
+            CdswEnvVar.TEST_EXECUTION_MODE.value, default_value=TestExecMode.CLOUDERA.value
+        )
+        return TestExecMode[exec_mode_env.upper()]
+
+    @classmethod
+    def get_cdsw_root_dir(cls):
         found_cdsw_dirs = FileUtils.find_files(
-            cls.repo_root_dir,
+            LocalDirs.REPO_ROOT_DIR,
             find_type=FindResultType.DIRS,
             regex=CDSW_DIRNAME,
             parent_dir="yarndevtools",
@@ -68,28 +182,7 @@ class YarnCdswBranchDiffTests(unittest.TestCase):
                 f"and parent dir 'yarndevtools'. "
                 f"Actual results: {found_cdsw_dirs}"
             )
-        cls.repo_cdsw_root_dir = found_cdsw_dirs[0]
-        cls.yarn_dev_tools_results_dir = FileUtils.join_path(cls.repo_cdsw_root_dir, "yarndevtools-results")
-        cls.branchdiff_cdsw_runner_script = YarnCdswBranchDiffTests.find_cdsw_runner_script(
-            os.path.join(cls.repo_cdsw_root_dir, BRANCH_DIFF_REPORTER_DIR_NAME)
-        )
-        cls.docker_test_setup = DockerTestSetup(
-            DOCKER_IMAGE, create_image=CREATE_IMAGE, dockerfile_location=cls.repo_cdsw_root_dir, logger=CMD_LOG
-        )
-
-        exec_mode_env: str = OsUtils.get_env_value(CdswEnvVar.TEST_EXECUTION_MODE.value, TestExecMode.CLOUDERA.value)
-        cls.exec_mode: TestExecMode = TestExecMode[exec_mode_env.upper()]
-
-        # !! WARNING: User-specific settings !!
-        if cls.exec_mode == TestExecMode.CLOUDERA:
-            # We need both upstream / downstream repos for Cloudera-mode
-            os.environ[CdswEnvVar.CLOUDERA_HADOOP_ROOT.value] = "/Users/snemeth/development/cloudera/hadoop/"
-            os.environ[CdswEnvVar.HADOOP_DEV_DIR.value] = "/Users/snemeth/development/apache/hadoop"
-        elif cls.exec_mode == TestExecMode.UPSTREAM:
-            os.environ[CdswEnvVar.HADOOP_DEV_DIR.value] = "/Users/snemeth/development/apache/hadoop"
-            os.environ[BranchComparatorEnvVar.REPO_TYPE.value] = RepoType.UPSTREAM.value
-            os.environ[BranchComparatorEnvVar.FEATURE_BRANCH.value] = "origin/branch-3.3"
-            os.environ[BranchComparatorEnvVar.MASTER_BRANCH.value] = "origin/trunk"
+        return found_cdsw_dirs[0]
 
     @classmethod
     def _setup_logging(cls):
@@ -106,65 +199,32 @@ class YarnCdswBranchDiffTests(unittest.TestCase):
             raise ValueError(f"Expected to find file: {CDSW_RUNNER_PY}")
         return results[0]
 
+    @classmethod
+    def exec_branch_diff_script(cls, args="", env: Dict[str, str] = None):
+        return cls.docker_test_setup.exec_cmd_in_container(
+            f"{PYTHON3} {ContainerFiles.BRANCH_DIFF_SCRIPT} {args}", stdin=False, tty=False, env=env
+        )
+
+    @classmethod
+    def exec_initial_cdsw_setup_script(cls, args="", env: Dict[str, str] = None):
+        if cls.python_module_mode == PythonModuleMode.GLOBAL:
+            args = PythonModuleMode.GLOBAL.value
+        return cls.docker_test_setup.exec_cmd_in_container(
+            f"{BASH} {ContainerFiles.INITIAL_CDSW_SETUP_SCRIPT} {args}", stdin=False, tty=False, env=env
+        )
+
     def setUp(self):
         self.docker_test_setup.test_instance = self
-
-    def _setup_default_docker_mounts(self):
-        # Mount dev dir so source code changes are visible in container immediately
-        self.docker_test_setup.mount_dir(self.repo_root_dir, YARN_DEV_TOOLS_MODULE_ROOT, mode=MOUNT_MODE_RW)
-
-        if self.exec_mode == TestExecMode.CLOUDERA:
-            self._mount_downstream_hadoop_repo()
-            self._mount_upstream_hadoop_repo()
-        elif self.exec_mode == TestExecMode.UPSTREAM:
-            self._mount_upstream_hadoop_repo()
-        # Mount results dir so all output files will be available on the host
-        self.docker_test_setup.mount_dir(
-            self.yarn_dev_tools_results_dir, YARN_DEV_TOOLS_OUTPUT_CONTAINER_DIR, mode=MOUNT_MODE_RW
-        )
-
-    def _mount_downstream_hadoop_repo(self):
-        # Mount local Cloudera Hadoop dir so that container won't clone it again and again
-        self.docker_test_setup.mount_dir(
-            os.environ[CdswEnvVar.CLOUDERA_HADOOP_ROOT.value], HADOOP_CLOUDERA_BASEDIR, mode=MOUNT_MODE_RW
-        )
-
-    def _mount_upstream_hadoop_repo(self):
-        # Mount local upstream Hadoop dir so that container won't clone it again and again
-        self.docker_test_setup.mount_dir(
-            os.environ[CdswEnvVar.HADOOP_DEV_DIR.value], HADOOP_UPSTREAM_BASEDIR, mode=MOUNT_MODE_RW
-        )
 
     def tearDown(self) -> None:
         self.docker_test_setup.cleanup()
 
     def save_latest_zip_from_container(self):
-        zip_link = FileUtils.join_path(self.yarn_dev_tools_results_dir, "latest-command-data-zip")
+        zip_link = FileUtils.join_path(LocalDirs.YARNDEVTOOLS_RESULT_DIR, "latest-command-data-zip")
         cont_target_path = os.readlink(zip_link)
-        local_target_path = FileUtils.join_path(self.yarn_dev_tools_results_dir, "latest-command-data-real.zip")
+        local_target_path = FileUtils.join_path(LocalDirs.YARNDEVTOOLS_RESULT_DIR, "latest-command-data-real.zip")
         command = f"docker cp {self.docker_test_setup.container.id}:{cont_target_path} {local_target_path}"
         SubprocessCommandRunner.run_and_follow_stdout_stderr(command)
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        pass
-
-    def exec_branch_diff_script(self, args="", env: Dict[str, str] = None):
-        return self.docker_test_setup.exec_cmd_in_container(
-            f"{PYTHON3} {BRANCH_DIFF_SCRIPT_CONTAINER} {args}", stdin=False, tty=False, env=env
-        )
-
-    def test_basic_cdsw_runner(self):
-        self._setup_default_docker_mounts()
-        self.docker_test_setup.run_container(sleep=CONTAINER_SLEEP)
-        # self.docker_test_setup.inspect_container(self.docker_test_setup.container.id)
-        exit_code = self.exec_branch_diff_script(env=self.cdsw_runner_env_dict())
-        self.assertEqual(exit_code, 0)
-        self.save_latest_zip_from_container()
-        # TODO check if zip exists and size is bigger than 0 and extractable
-        # TODO verify files are placed to correct dir in zip
-        # TODO verify if all files are present and they are non-zero sized
-        # TODO verify if HTML output is contained in email's body
 
     @classmethod
     def cdsw_runner_env_dict(cls):
@@ -180,7 +240,7 @@ class YarnCdswBranchDiffTests(unittest.TestCase):
             ]
         }
         # Manually fix PYTHONPATH like CDSW init script does
-        env_dict.update([cls.create_python_path_env_var(YARN_DEV_TOOLS_MODULE_ROOT)])
+        env_dict.update([cls.create_python_path_env_var(CommonDirs.YARN_DEV_TOOLS_MODULE_ROOT)])
         return env_dict
 
     @staticmethod
@@ -192,6 +252,19 @@ class YarnCdswBranchDiffTests(unittest.TestCase):
             new_pythonpath = new_dir
         return CdswEnvVar.PYTHONPATH.value, new_pythonpath
 
+    def test_basic_cdsw_runner(self):
+        self.docker_mounts.setup_default_docker_mounts()
+        self.docker_test_setup.run_container(sleep=CONTAINER_SLEEP)
+        self.exec_initial_cdsw_setup_script()
+        # self.docker_test_setup.inspect_container(self.docker_test_setup.container.id)
+        exit_code = self.exec_branch_diff_script(env=self.cdsw_runner_env_dict())
+        self.assertEqual(exit_code, 0)
+        self.save_latest_zip_from_container()
+        # TODO check if zip exists and size is bigger than 0 and extractable
+        # TODO verify files are placed to correct dir in zip
+        # TODO verify if all files are present and they are non-zero sized
+        # TODO verify if HTML output is contained in email's body
+
     def test_streaming_cmd_output(self):
         captured_output = []
 
@@ -202,13 +275,13 @@ class YarnCdswBranchDiffTests(unittest.TestCase):
                 pid = docker_setup.exec_cmd_in_container(f"pgrep -f {os.path.basename(cmd)}", stream=False)
                 docker_setup.exec_cmd_in_container(f"kill {pid}", stream=False)
 
-        self._setup_default_docker_mounts()
+        self.docker_mounts.setup_default_docker_mounts()
         self.docker_test_setup.run_container()
         self.docker_test_setup.exec_cmd_in_container(
-            f"{CDSW_BASEDIR}/common/test.sh", callback=_kill_after_5_lines, fail_on_error=False
+            f"{ContainerDirs.CDSW_BASEDIR}/common/test.sh", callback=_kill_after_5_lines, fail_on_error=False
         )
         self.docker_test_setup.exec_cmd_in_container(
-            f"{PYTHON3} {CDSW_BASEDIR}/common/test.py", callback=_kill_after_5_lines, fail_on_error=False
+            f"{PYTHON3} {ContainerDirs.CDSW_BASEDIR}/common/test.py", callback=_kill_after_5_lines, fail_on_error=False
         )
 
     # TODO write testcase to test ut-results-reporting with fake jenkins: It can return a valid & invalid UT result JSON response
