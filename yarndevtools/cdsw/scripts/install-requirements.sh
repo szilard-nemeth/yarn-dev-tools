31ae7d917b76a05a3682e87b0f957def7bb03112
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-11-04 11:51:02 +0100

    install-requirements.sh: Remove --force-reinstall flag from pip3 install

--- yarndevtools/cdsw/scripts/install-requirements.sh
+++ yarndevtools/cdsw/scripts/install-requirements.sh
@@ -3,4 +3,4 @@
 # Install python requirements with the latest version of the requirements file
 ##NOTE: yarndevtools will be installed as a python module so it won't fail with:
 curl -o /tmp/requirements-cdsw.txt https://raw.githubusercontent.com/szilard-nemeth/yarn-dev-tools/master/yarndevtools/cdsw/requirements.txt
-pip3 install -r /tmp/requirements-cdsw.txt --force-reinstall
+pip3 install -r /tmp/requirements-cdsw.txt
