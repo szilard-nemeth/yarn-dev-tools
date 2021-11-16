0a1be2fada84f99ede3e8013a0e1dde3420f4cc3
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-11-16 15:17:25 +0100

    cdsw_runner / Jenkins test reporter: Use new CLI flags

--- yarndevtools/cdsw/unit-test-result-reporting/cdsw_runner.py
+++ yarndevtools/cdsw/unit-test-result-reporting/cdsw_runner.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import logging
-from typing import List
 
 from pythoncommons.os_utils import OsUtils
 
-from yarndevtools.argparser import CommandType
+from yarndevtools.argparser import CommandType, JENKINS_BUILDS_EXAMINE_UNLIMITIED_VAL
 from yarndevtools.cdsw.common_python.cdsw_common import (
     CdswRunnerBase,
     CdswSetup,
@@ -28,7 +27,15 @@ class CdswRunner(CdswRunnerBase):
         self.run_clone_downstream_repos_script(basedir)
         self.run_test_reporter(mode=JenkinsTestReporterMode.MAWO)
 
-    def run_test_reporter(self, mode: JenkinsTestReporterMode, recipients=None, testcase_filter: str = TC_FILTER_ALL):
+    def run_test_reporter(
+        self,
+        mode: JenkinsTestReporterMode,
+        recipients=None,
+        testcase_filter: str = TC_FILTER_ALL,
+        num_builds: str = JENKINS_BUILDS_EXAMINE_UNLIMITIED_VAL,
+        omit_job_summary: bool = False,
+        download_uncached_job_data: bool = True,
+    ):
         if not mode:
             raise ValueError("Jenkins job mode should be specified!")
 
@@ -37,15 +44,19 @@ class CdswRunner(CdswRunnerBase):
         process_builds: int = OsUtils.get_env_value(JenkinsTestReporterEnvVar.BUILD_PROCESSING_LIMIT.value, 1)
         LOG.info(f"Processing {process_builds} builds...")
         sender = "YARN jenkins test reporter"
-        tc_filter_param = f"--testcase-filter {testcase_filter}"
+        omit_job_summary_param = "--omit-job-summary" if omit_job_summary else ""
+        download_uncached_job_data_param = "--download-uncached-job-data" if download_uncached_job_data else ""
         self.execute_yarndevtools_script(
             f"--debug {CommandType.JENKINS_TEST_REPORTER.name} "
-            f"--mode {mode} "
+            f"--mode {mode.mode_name} "
             f"{self.common_mail_config.as_arguments()}"
             f'--sender "{sender}" '
             f'--recipients "{recipients}" '
-            f"{tc_filter_param} "
-            f"--request-limit {process_builds}"
+            f"--testcase-filter {testcase_filter} "
+            f"--request-limit {process_builds} "
+            f"--num-builds {num_builds} "
+            f"{omit_job_summary_param} "
+            f"{download_uncached_job_data_param} "
         )
 
 
