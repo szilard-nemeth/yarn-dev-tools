1c3a4dba8ed035902eed9c882f4ce23aa4f3e775
Szilard Nemeth <szilard.nemeth88@gmail.com>
2021-12-08 01:02:54 +0100

    CdswRunnerBase: Implemented more sophisticated restart mechanism for CDSW

--- yarndevtools/cdsw/common_python/cdsw_common.py
+++ yarndevtools/cdsw/common_python/cdsw_common.py
@@ -285,24 +285,54 @@ class CdswRunnerBase(ABC):
         LOG.info("Setup result: %s", setup_result)
         self.cdsw_runner_script_path = cdsw_runner_script_path
         self.start_date_str = self.current_date_formatted()
-
-        # CDSW env values:
-        # sys.executable: /usr/local/bin/python3
-        # sys.argv: ['/usr/local/bin/ipython3']
-        # final command: ['/usr/local/bin/python3.8',
-        #           '/home/cdsw/.local/lib/python3.8/site-packages/yarndevtools/cdsw/unit-test-result-reporting/cdsw_runner.py']
         if setup_result.install_requirements_invoked and CdswRunnerBase._is_env_var_true(
             CdswEnvVar.RESTART_PROCESS_WHEN_REQUIREMENTS_INSTALLED.value, default_val=True
         ):
-            final_command = [sys.executable, self.cdsw_runner_script_path]
-            LOG.info(
-                "Restarting Python process. sys.executable: %s, sys.argv: %s, final command: %s",
-                sys.executable,
-                sys.argv,
-                final_command,
-            )
-            OsUtils.set_env_value(CdswEnvVar.INSTALL_REQUIREMENTS.value, False)
-            os.execvp(sys.executable, final_command)
+            self.restart_execution()
+
+    def restart_execution(self):
+        """
+         Variable values in case of CDSW run:
+         1. sys.executable: /usr/local/bin/python3
+         2. sys.argv: ['/usr/local/bin/ipython3']
+         3. final command: ['/usr/local/bin/python3',
+                   '/home/cdsw/.local/lib/python3.8/site-packages/yarndevtools/cdsw/unit-test-result-reporting/cdsw_runner.py']
+
+         Variable values under normal run (e.g. from test_branchdiff_reporter.py):
+         1. sys.executable: /usr/local/bin/python3
+         2. sys.argv: ['/home/cdsw/jobs/downstream-branchdiff-reporting/cdsw_runner.py']
+         3. final command: ['/usr/local/bin/python3', '/usr/local/lib/python3.8/site-packages/yarndevtools/cdsw/downstream-branchdiff-reporting/cdsw_runner.py']
+        :return:
+        """
+
+        # Let's pick two random environment variables that are starting with name "CDSW_" to decide if we are running on CDSW
+        real_cdsw_env = OsUtils.get_env_value("CDSW_PROJECT", None) and OsUtils.get_env_value("CDSW_ENGINE_URL", None)
+        if real_cdsw_env:
+            LOG.info("Detected real CDSW environment")
+            if not sys.argv:
+                raise ValueError("Was expecting sys.argv to be not empty! Current value: {}".format(sys.argv))
+            elif len(sys.argv) > 1:
+                raise ValueError("Was expecting sys.argv to have a length of 1! Current value: {}".format(sys.argv))
+            argv0 = sys.argv[0]
+            if "ipython" not in argv0:
+                raise ValueError("Was expecting sys.argv[] to contain 'ipython'! Current value: {}".format(argv0))
+            executable = argv0
+            command_args = [self.cdsw_runner_script_path]
+        else:
+            LOG.info("Detected artificial CDSW environment")
+            executable = sys.executable
+            command_args = [self.cdsw_runner_script_path]
+
+        LOG.info(
+            "Restarting Python process. " "sys.executable: %s, sys.argv: %s, executable: %s, command args: %s",
+            sys.executable,
+            sys.argv,
+            executable,
+            command_args,
+        )
+        # Prevent running the restart process forever
+        OsUtils.set_env_value(CdswEnvVar.RESTART_PROCESS_WHEN_REQUIREMENTS_INSTALLED.value, False)
+        os.execvp(executable, command_args)
 
     @abstractmethod
     def start(self, basedir, cdsw_runner_script_path: str):
