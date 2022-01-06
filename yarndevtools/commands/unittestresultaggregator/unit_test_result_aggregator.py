d9d3b60839ad149333cbfb413bebce51f7122e24
Szilard Nemeth <szilard.nemeth88@gmail.com>
2022-01-06 19:01:06 +0100

    FIX: Unresolved references

--- yarndevtools/commands/unittestresultaggregator/unit_test_result_aggregator.py
+++ yarndevtools/commands/unittestresultaggregator/unit_test_result_aggregator.py
@@ -15,6 +15,7 @@ from pythoncommons.os_utils import OsUtils
 from pythoncommons.project_utils import ProjectUtils
 from pythoncommons.string_utils import RegexUtils
 
+from tests.cdsw.common.testutils.cdsw_testing_common import SECRET_PROJECTS_DIR
 from yarndevtools.commands.unittestresultaggregator.common import (
     MATCH_ALL_LINES_EXPRESSION,
     MATCHTYPE_ALL_POSTFIX,
@@ -33,7 +34,7 @@ from yarndevtools.commands.unittestresultaggregator.common import (
     BuildComparisonResult,
 )
 from yarndevtools.commands.unittestresultaggregator.representation import SummaryGenerator, UnitTestResultOutputManager
-from yarndevtools.common.shared_command_utils import SECRET_PROJECTS_DIR, CommandType
+from yarndevtools.common.shared_command_utils import CommandType
 
 VALID_OPERATION_MODES = [OperationMode.PRINT, OperationMode.GSHEET]
 
