eeed0ee9f18842bf25ce87d2a95dd45d9f6ab4c5
Szilard Nemeth <szilard.nemeth88@gmail.com>
2022-02-23 00:11:15 +0100

    review_sheet_backport_updater_job_config.py: Use 'summary.html' as email body file

--- yarndevtools/cdsw/job_configs/review_sheet_backport_updater_job_config.py
+++ yarndevtools/cdsw/job_configs/review_sheet_backport_updater_job_config.py
@@ -43,7 +43,7 @@ config = {
                 "enabled": True,
                 "send_attachment": False,
                 "attachment_file_name": lambda conf: f"{conf.var('commandDataFileName')}",
-                "email_body_file_from_command_data": "report-short.html",
+                "email_body_file_from_command_data": "summary.html",
                 "sender": lambda conf: f"{conf.var('sender')}",
                 "subject": lambda conf: f"{conf.var('subject')}",
             },
