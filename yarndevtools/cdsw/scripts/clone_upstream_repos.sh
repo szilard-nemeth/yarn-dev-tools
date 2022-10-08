9b009af23d4a06ee6da4b036e40d8e086e5a0a9b
Szilard Nemeth <szilard.nemeth88@gmail.com>
2022-10-08 16:25:36 +0200

    Fix: Do not do hard git reset when there are changes in downstream / upstream repo

--- yarndevtools/cdsw/scripts/clone_upstream_repos.sh
+++ yarndevtools/cdsw/scripts/clone_upstream_repos.sh
@@ -13,8 +13,17 @@ function clone-fetch-hadoop() {
   curr_ref=$(git rev-parse HEAD)
   origin_trunk_curr_ref=$(git rev-parse $TRUNK_BRANCH)
   if [ "$curr_ref" != "$origin_trunk_curr_ref" ]; then
-    echo "Resetting to $TRUNK_BRANCH..."
-    git reset --hard $TRUNK_BRANCH
+    if [ -z ${TEST_EXEC_MODE+x} ]; then
+      echo "Test exec mode not set, resetting to $CDPD_MASTER_BRANCH with git reset --hard..."
+      git reset --hard $CDPD_MASTER_BRANCH
+    else
+      echo "Test exec mode set, resetting to $CDPD_MASTER_BRANCH with git reset..."
+      if [[ -z $(git status -s) ]]; then
+        echo "There are unstaged changes in repo `pwd`. Exiting"
+        return 1
+      fi
+      git reset $CDPD_MASTER_BRANCH
+    fi
   fi
 
   set +e
