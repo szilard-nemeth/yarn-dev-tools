3a738fdb594e47a46c25247597b359c890967761
Szilard Nemeth <szilard.nemeth88@gmail.com>
2022-12-30 20:27:54 +0100

    JenkinsJobBuildDataAndEmailContentAggregator: Do not store dates as FinalAggregationResults also does the same

--- yarndevtools/commands/unittestresultaggregator/db/aggregation.py
+++ yarndevtools/commands/unittestresultaggregator/db/aggregation.py
@@ -19,8 +19,6 @@ class JenkinsJobBuildDataAndEmailContentAggregator:
         self.fetcher_data_dict: Dict[str, Dict[str, JobBuildData]] = {}
         # key: job name, value: Dict[build number, EmailContent]
         self.aggregator_data_dict: Dict[str, Dict[str, EmailContent]] = {}
-        # key: job name, value: set of dates
-        self.dates: Dict[str, Set[datetime.date]] = {}
 
     def aggregate(self, result: AggregationResults):
         """
@@ -44,10 +42,10 @@ class JenkinsJobBuildDataAndEmailContentAggregator:
         for bd in build_data:
             builds_per_job = self.fetcher_data_dict.setdefault(bd.job_name, {})
 
-            if (bd.job_name, bd.build_number) not in processed:
+            key = (bd.job_name, bd.build_number)
+            if key not in processed:
                 builds_per_job.setdefault(bd.build_number, bd)
-                self.dates.setdefault(bd.job_name, set()).add(bd.build_datetime.date())
-                processed.add((bd.job_name, bd.build_number))
+                processed.add(key)
             else:
                 LOG.debug("%s is already processed, not storing build again")
 
@@ -63,7 +61,6 @@ class JenkinsJobBuildDataAndEmailContentAggregator:
             key = (ec.job_name, ec.build_number)
             if key not in processed:
                 builds_per_job.setdefault(ec.build_number, ec)
-                self.dates.setdefault(ec.job_name, set()).add(ec.date.date())
                 processed.add(key)
             else:
                 LOG.debug("%s is already processed, not storing build again", key)
@@ -83,8 +80,6 @@ class JenkinsJobBuildDataAndEmailContentAggregator:
 
         processed: Set[Tuple[str, str]] = set()  # tuple: (job name, build number)
 
-        self._print_stats()
-
         # TODO avoid code duplication
         for job_name, inner_dict in self.aggregator_data_dict.items():
             for build_number, job_build_data in inner_dict.items():
@@ -102,22 +97,7 @@ class JenkinsJobBuildDataAndEmailContentAggregator:
                     # TODO yarndevtoolsv2: Log something
 
         result.finish_processing()
-
-        # TODO yarndevtoolsv2: These 2 should be the same, printout should not happen in this class!
-        #  DEBUG why these 2 dicts are not equal!
-        #  Save email meta to DB // store dates of emails as well to mongodb:
-        #  Write start date, end date, missing dates between start and end date
-        #  builds_with_dates = self._aggregation_results._failed_builds.get_dates()
-        #  Cross-check date-related functionality with JenkinsJobBuildDataAndEmailContentJoiner
-
-        tmp = {}
-        for job_name, dates in self.dates.items():
-            tmp[job_name] = sorted(list(dates), reverse=True)
-        self.dates = tmp
-
-        dates1 = result._aggregation_results._failed_builds.get_unique_dates()
-        dates2 = self.dates
-        assert dates1 == dates2, "Date dictionaries are not equal"
+        self._print_stats(result)
 
     def _is_aggregator_only(self, job_name, build_number):
         if job_name in self.aggregator_only and build_number in self.aggregator_only[job_name]:
@@ -145,7 +125,7 @@ class JenkinsJobBuildDataAndEmailContentAggregator:
         return res
 
     @staticmethod
-    # TODO: this more abstract and move to pythoncommons?
+    # TODO: Make this more abstract and move to pythoncommons?
     def _get_from_both_dicts(dic_a, dic_b):
         res: Dict[str, Set[str]] = {}
         for job_name, inner_dict in dic_a.items():
@@ -159,8 +139,8 @@ class JenkinsJobBuildDataAndEmailContentAggregator:
                     res.setdefault(job_name, set()).add(build_number)
         return res
 
-    def _print_stats(self):
-        self._print_date_stats()
+    def _print_stats(self, result: AggregationResults):
+        result.print_date_stats()
         aggregator_only_formatted = pformat(self.aggregator_only)
         fetcher_only_formatted = pformat(self.fetcher_only)
         from_both_formatted = pformat(self.from_both)
@@ -173,24 +153,6 @@ class JenkinsJobBuildDataAndEmailContentAggregator:
             fetcher_only_formatted,
             from_both_formatted,
         )
-
-    def _print_date_stats(self):
-        LOG.debug("Printing date statistics. ")
-        for job_name, dates in self.dates.items():
-            sorted_dates = sorted(dates)
-            first_date = sorted_dates[0]
-            last_date = sorted_dates[-1]
-            all_no_of_days = (last_date - first_date).days
-            missing_dates = DateUtils.get_missing_dates(sorted_dates)
-            LOG.debug(
-                "Job: %s, All days: %s, First date: %s, Last date: %s, Sorted dates: %s, Missing dates: %s",
-                job_name,
-                all_no_of_days,
-                first_date,
-                last_date,
-                pformat(sorted_dates),
-                pformat(missing_dates),
-            )
 
     @staticmethod
     def _process_failed_build(result: AggregationResults, failed_build: FailedBuildAbs):
