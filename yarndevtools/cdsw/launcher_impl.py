fe3eabf320f48c2020b3ef0a26b7e8c754cbcc96
Szilard Nemeth <szilard.nemeth88@gmail.com>
2024-08-03 01:38:18 -0400

    Moving cdsw from yarndevtools: Implement scripts_to_execute in YarnDevToolsCdswApp

--- yarndevtools/cdsw/launcher_impl.py
+++ yarndevtools/cdsw/launcher_impl.py
@@ -1,6 +1,23 @@
+import enum
+
 from cdswjoblauncher.contract import CdswApp, CdswSetupInput
+
+
+class CdswExecutionMode(enum.Enum):
+    CLOUDERA = "cloudera"
+    UPSTREAM = "upstream"
 
 
 class YarnDevToolsCdswApp(CdswApp):
     def scripts_to_execute(self, cdsw_input: CdswSetupInput):
-        raise NotImplementedError()
+        try:
+            exec_mode = CdswExecutionMode[cdsw_input.execution_mode.upper()]
+        except ValueError:
+            raise ValueError(f"Invalid value for execution mode: {cdsw_input.execution_mode}")
+
+        if exec_mode == CdswExecutionMode.CLOUDERA:
+            return ["clone_upstream_repos.sh", "clone_downstream_repos.sh"]
+        elif exec_mode == CdswExecutionMode.UPSTREAM:
+            return ["clone_downstream_repos.sh"]
+        else:
+            raise ValueError(f"Unhandled exec mode: {exec_mode}")
